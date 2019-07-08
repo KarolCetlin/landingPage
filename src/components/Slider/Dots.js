70e1be91f0084ea4755e431267abb854bca43752
Karol Cetlin <cetlin.karol@gmail.com>
2019-07-08 10:58:33 +0200

    dots, keydown work

--- src/components/Slider/Dots.js
+++ src/components/Slider/Dots.js
@@ -2,48 +2,20 @@ import React from 'react'
 import styles from './Slider.module.scss'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faCircle} from '@fortawesome/free-solid-svg-icons'
-import {SliderData} from './SliderData'
-import Slider from './Slider'
 
-class Dots extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      dotIndex: 0,
-    }
-
-    // this.dotsClick = this.dotsClick.bind(this)
-  }
-
-  // dotsClick(index) {
-  //   console.log(index)
-  //   this.setState({
-  //     dotIndex: index,
-  //   })
-  // }
-
-  render() {
-    // const props = {
-    //   ...this.props,
-    // }
-
-    /* onClick={() => this.dotsClick(index)} */
-
-    return (
-      <>
-        <section className={styles.slider__dotsPosition}>
-          {SliderData.map((item, index) => (
-            <FontAwesomeIcon
-              className={styles.slider__dots_item}
-              icon={faCircle}
-              key={index}
-            />
-          ))}
-        </section>
-      </>
-    )
-  }
-}
+const Dots = ({onClick, count}) => (
+  <section className={styles.slider__dotsPosition}>
+    {Array(count)
+      .fill(null)
+      .map((item, index) => (
+        <FontAwesomeIcon
+          className={styles.slider__dots_item}
+          icon={faCircle}
+          key={index}
+          onClick={onClick && (() => onClick(index))}
+        />
+      ))}
+  </section>
+)
 
 export default Dots
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -5,68 +5,62 @@ import Arrows from './Arrows.js'
 import Dots from './Dots.js'
 
 class Slider extends React.Component {
+  intervalId
+
   constructor(props) {
     super(props)
 
     this.state = {
-      initialSlideNumber: 0,
+      currentSlideIndex: 0,
     }
 
-    this.runSlideShow = this.runSlideShow.bind(this)
     this.nextSlide = this.nextSlide.bind(this)
-    this.previousSlide = this.previousSlide.bind(this)
-    this.dotsClick = this.dotsClick.bind(Dots)
+    this.prevSlide = this.prevSlide.bind(this)
+    this.handleDotsClick = this.handleDotsClick.bind(Dots)
   }
 
   componentDidMount() {
     this.runSlideShow()
 
-    document.addEventListener('keydown', this.keyboardSupport)
+    document.addEventListener('keydown', ({key}) => this.handleKeydown(key))
   }
 
   runSlideShow() {
-    let intervalId = setInterval(
+    this.intervalId = setInterval(
       this.nextSlide,
-      SliderData[this.state.initialSlideNumber].displayTime,
+      SliderData[this.state.currentSlideIndex].displayTime,
     )
-
-    this.setState({
-      intervalId,
-    })
   }
 
   nextSlide() {
     this.setState({
-      initialSlideNumber:
-        (this.state.initialSlideNumber + 1) % SliderData.length,
+      currentSlideIndex: (this.state.currentSlideIndex + 1) % SliderData.length,
     })
   }
 
-  previousSlide() {
-    if (this.state.initialSlideNumber === 0) {
-      this.setState({
-        initialSlideNumber: (this.state.initialSlideNumber = SliderData.length),
-      })
+  prevSlide() {
+    const currentSlideIndex =
+      0 === this.state.currentSlideIndex
+        ? SliderData.length - 1
+        : this.state.currentSlideIndex - 1
+
+    this.setState({currentSlideIndex})
+  }
+
+  handleDotsClick(index) {
+    console.log(index)
+  }
+
+  handleKeydown(key) {
+    switch (key) {
+      case 'ArrowLeft':
+        this.prevSlide()
+        break
+
+      case 'ArrowRight':
+        this.nextSlide()
+        break
     }
-
-    this.setState({
-      initialSlideNumber:
-        (this.state.initialSlideNumber - 1) % SliderData.length,
-    })
-  }
-
-  dotsClick() {
-    console.log('asd')
-  }
-
-  // keyboardSupport(event){
-  //   event.keyCode === 37
-  //     ? this.previousSlide()
-  //     : event.keyCode === 39 ? this.nextSlide() : null;
-  // }
-
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.keyboardSupport)
   }
 
   render() {
@@ -74,28 +68,23 @@ class Slider extends React.Component {
       ...this.state,
     }
     return (
-      <>
-        <section className={styles.slider}>
-          <h1 className={styles.slider__title} id="sliderMainTitle">
-            {SliderData[sliderNumber.initialSlideNumber].mainTitle}
-          </h1>
-          <h2 className={styles.slider__subtitle} id="sliderSubTitle">
-            {SliderData[sliderNumber.initialSlideNumber].subTitle}
-          </h2>
-          <img
-            className={styles.slider__picture}
-            id="sliderPicture"
-            src={SliderData[sliderNumber.initialSlideNumber].img}
-            alt=""
-          />
-          <Dots onClick={this.dotsClick} />
+      <section className={styles.slider}>
+        <h1 className={styles.slider__title} id="sliderMainTitle">
+          {SliderData[sliderNumber.currentSlideIndex].mainTitle}
+        </h1>
+        <h2 className={styles.slider__subtitle} id="sliderSubTitle">
+          {SliderData[sliderNumber.currentSlideIndex].subTitle}
+        </h2>
+        <img
+          className={styles.slider__picture}
+          id="sliderPicture"
+          src={SliderData[sliderNumber.currentSlideIndex].img}
+          alt=""
+        />
+        <Dots onClick={this.handleDotsClick} count={SliderData.length} />
 
-          <Arrows
-            nextSlide={this.nextSlide}
-            previousSlide={this.previousSlide}
-          />
-        </section>
-      </>
+        <Arrows nextSlide={this.nextSlide} previousSlide={this.prevSlide} />
+      </section>
     )
   }
 }
